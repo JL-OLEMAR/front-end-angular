c1a2b954e28f71894af9ca217f068ab359fe1a4b
jose <joseluis19963@gmail.com>
2020-10-03 18:22:48 -0500

    Librerias 1.0

---
+++ src/app/components/user-edit/user-edit.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
+})
+export class UserEditComponent implements OnInit {
+  public page_title: string;
+  public user: User;
+  public identity;
+  public token;
+  public status;
+
+  constructor(
+    private _userService: UserService
+  ) {
+    this.page_title = 'Ajustes de usuario';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.gettoken();
+
+    //Rellenar el objeto usuario
+    this.user = new User(
+      this.identity.sub,
+      this.identity.name,
+      this.identity.surname,
+      this.identity.role,
+      this.identity.email,
+      '',
+      this.identity.description,
+      this.identity.image
+    );
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form) {
+    this._userService.update(this.token, this.user).subscribe(
+      response => {
+        if (response && response.status) {
+          console.log(response);
+          this.status = 'success';
+
+          //Actualizar usuario en sesión
+          if (response.changes.name) {
+            this.user.name = response.changes.name;
+          }
+          if (response.changes.surname) {
+            this.user.surname = response.changes.surname;
+          }
+          if (response.changes.email) {
+            this.user.email = response.changes.email;
+          }
+          if (response.changes.description) {
+            this.user.description = response.changes.description;
+          }
+          if (response.changes.image) {
+            this.user.image = response.changes.image;
+          }
+          this.identity = this.user;
+          localStorage.setItem('identity', JSON.stringify(this.identity));
+
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    );
+  }
+
+}
